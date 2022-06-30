44db4df1226806f55ad9b4a5c4bd0c11953abaee
Peculiar <Peculiar907@gmail.com>
2022-06-30 09:27:05 -0700

    Write a C program that prints

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of any C program by javapoint
+ *
+ * Return: returns 1 which indicate failure
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
