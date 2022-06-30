523bf6b3059ce9dc88dd25cbbb232787e025bcdd
Peculiar <Peculiar907@gmail.com>
2022-06-30 09:02:43 -0700

    C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point of any C program by javapoint
+ *
+ * Return: Consistently 0 which indicate Success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
