214087df05e3c2ca107b3b1006b366e125cc4e22
Peculiar <Peculiar907@gmail.com>
2022-07-05 13:16:00 -0700

    t0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	_putchar(4);
-	_putchar(2);
-	_putchar(6);
-	_putchar(8);
-	_putchar(12);
-	_putchar(14);
-	_putchar(16);
-	_putchar(18);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
