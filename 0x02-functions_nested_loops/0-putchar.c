a56dad11bfd4050efac9814af2f1182c548b4614
Peculiar <Peculiar907@gmail.com>
2022-07-05 08:11:43 -0700

    t0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *Return: always 0
+ */
+int main(void)
+{
+	_putchar(4);
+	_putchar(2);
+	_putchar(6);
+	_putchar(8);
+	_putchar(12);
+	_putchar(14);
+	_putchar(16);
+	_putchar(18);
+	_putchar('\n');
+
+	return (0);
+}
