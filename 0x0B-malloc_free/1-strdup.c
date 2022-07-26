75912f9a9a68cc6b1e0644e3de3c7bfacbe131a8
Peculiar <Peculiar907@gmail.com>
2022-07-26 11:13:42 -0700

    Task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup -pointer to a new string which is a duplicate of the string str
+ *
+ * @str: string to copy
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	unsigned int a, b;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (a = 0; str[a]; a++)
+		;
+		a++;
+	s = malloc(a * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; b < i; b++)
+	{
+	s[b] = str[b];
+	}
+	return (s);
+}
